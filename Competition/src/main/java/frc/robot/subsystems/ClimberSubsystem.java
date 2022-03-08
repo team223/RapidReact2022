2e7bae519c104420a4f1f71b73dbdea1ad3cfd3b
XtremeHeat <frcteam223@gmail.com>
2022-03-08 11:34:57 -0500

    Auto Sequence working TODO: end when button

--- Competition/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
+++ Competition/src/main/java/frc/robot/subsystems/ClimberSubsystem.java
@@ -4,30 +4,91 @@
 
 package frc.robot.subsystems;
 
+import java.util.Set;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ScheduleCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+import frc.robot.commands.DriveClimbers;
+import frc.robot.commands.WaitFor;
 
 public class ClimberSubsystem extends SubsystemBase {
   /** Creates a new Climber. */
   private static CANSparkMax leftClimber = new CANSparkMax( Constants.LEFT_CLIMBER_ID, MotorType.kBrushless );
- private static CANSparkMax rightClimber = new CANSparkMax( Constants.RIGHT_CLIMBER_ID, MotorType.kBrushless );
-
+  private static CANSparkMax rightClimber = new CANSparkMax( Constants.RIGHT_CLIMBER_ID, MotorType.kBrushless );
 
   private static Solenoid actuation = new Solenoid( 1, PneumaticsModuleType.REVPH, 6 );
   
-  public ClimberSubsystem() {}
+  public ClimberSubsystem() {
+    resetPosition();
+    actuation.set( true );
+  }
 
   public void toggleSolenoid( ){
     actuation.toggle();
   }
 
+  public void resetPosition(){
+    leftClimber.getEncoder().setPosition( 0 );
+    rightClimber.getEncoder().setPosition( 0 );
+  }
+
+  public double getPosition(){
+    return -leftClimber.getEncoder().getPosition();
+  }
+
   public void setClimbers( double speed ){
     leftClimber.set( speed );
+    rightClimber.set( -speed );
+  
+    System.out.println( "Climbers: " + leftClimber.getEncoder().getPosition() + ", " + rightClimber.getEncoder().getPosition() );
+  }
+
+  private  SequentialCommandGroup group  = new SequentialCommandGroup(
+    new DriveClimbers( 60 ),
+    new InstantCommand(){
+      @Override
+      public void execute(){
+        toggleSolenoid();
+      }
+    },
+    new DriveClimbers( 190 ),
+    new InstantCommand(){
+      @Override
+      public void execute(){
+        toggleSolenoid();
+      }
+    }       
+  );;
+  public void runClimbers(){
+    
+
+      group.schedule();
+  }
+
+  public void ohShit(){
+    try{
+      group.e
+    }catch( Exception e ){
+      
+    }
+  }
+
+  public void setLeftClimber( double speed ){
+    leftClimber.set( speed );
+  }
+
+  public void setRightClimber( double speed ){
     rightClimber.set( -speed );
   }
 
